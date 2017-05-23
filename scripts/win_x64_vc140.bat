5247b443b03ce7469185e5d6afb295e29df6c863
Clark Rinker <cgrinker@gmail.com>
2017-05-23 16:08:31 -0700

    Refactoring Dependency to use more meaningful ```dist\``` output directory

--- scripts/win_x64_vc140.bat
+++ scripts/win_x64_vc140.bat
@@ -9,18 +9,19 @@ msbuild craft_util.vcxproj /t:Build /p:Configuration=Debug /property:Platform=x6
 msbuild craft_util.vcxproj /t:Build /p:Configuration=Release /property:Platform=x64
 
 
-mkdir %PROJECT_ROOT%\out\include
-mkdir %PROJECT_ROOT%\out\bin\%TRIPLE_CROSS%\debug
-mkdir %PROJECT_ROOT%\out\lib\%TRIPLE_CROSS%\debug
+mkdir %PROJECT_ROOT%\dist\include
+mkdir %PROJECT_ROOT%\dist\bin\%TRIPLE_CROSS%\debug
+mkdir %PROJECT_ROOT%\dist\lib\%TRIPLE_CROSS%\debug
 
-COPY %PROJECT_ROOT%\build\Debug\craft_util.dll %PROJECT_ROOT%\out\bin\%TRIPLE_CROSS%\debug\
-COPY %PROJECT_ROOT%\build\Debug\craft_util.pdb %PROJECT_ROOT%\out\bin\%TRIPLE_CROSS%\debug\
-COPY %PROJECT_ROOT%\build\Debug\craft_util.lib %PROJECT_ROOT%\out\lib\%TRIPLE_CROSS%\debug\
+COPY %PROJECT_ROOT%\build\Debug\craft_util.dll %PROJECT_ROOT%\dist\bin\%TRIPLE_CROSS%\debug\
+COPY %PROJECT_ROOT%\build\Debug\craft_util.pdb %PROJECT_ROOT%\dist\bin\%TRIPLE_CROSS%\debug\
+COPY %PROJECT_ROOT%\build\Debug\craft_util.lib %PROJECT_ROOT%\dist\lib\%TRIPLE_CROSS%\debug\
 
-COPY %PROJECT_ROOT%\build\Release\craft_util.dll %PROJECT_ROOT%\out\bin\%TRIPLE_CROSS%\
-COPY %PROJECT_ROOT%\build\Release\craft_util.pdb %PROJECT_ROOT%\out\bin\%TRIPLE_CROSS%\
-COPY %PROJECT_ROOT%\build\Release\craft_util.lib %PROJECT_ROOT%\out\lib\%TRIPLE_CROSS%\
+COPY %PROJECT_ROOT%\build\Release\craft_util.dll %PROJECT_ROOT%\dist\bin\%TRIPLE_CROSS%\
+COPY %PROJECT_ROOT%\build\Release\craft_util.pdb %PROJECT_ROOT%\dist\bin\%TRIPLE_CROSS%\
+COPY %PROJECT_ROOT%\build\Release\craft_util.lib %PROJECT_ROOT%\dist\lib\%TRIPLE_CROSS%\
 
-ROBOCOPY /e %PROJECT_ROOT%\src  %PROJECT_ROOT%\out\include\util\ *.h *.hpp
+ROBOCOPY /e %PROJECT_ROOT%\src  %PROJECT_ROOT%\dist\include\util\ *.h *.hpp
 
 cd %PROJECT_ROOT%
+7z a -tzip craft-util.zip dist
