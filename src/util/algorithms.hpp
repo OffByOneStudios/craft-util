a51f4b4455929462c9bc1cb5c6133ec13e2a23cb
Mason Bially <mason.bially@gmail.com>
2017-10-25 22:06:43 -0700

    Adding get_with_default.

--- src/util/algorithms.hpp
+++ src/util/algorithms.hpp
@@ -138,6 +138,18 @@ namespace stdext
 		return { cn };
 	}
 
+	//
+	// Dictionary helpers
+	//
+
+	template <template<class, class, class...> class C, typename K, typename V, typename... Args>
+	inline V get_with_default(const C<K, V, Args...>& m, K const& key, const V & defval)
+	{
+		typename C<K, V, Args...>::const_iterator it = m.find(key);
+		if (it == m.end())
+			return defval;
+		return it->second;
+	}
 
 	//
 	// String trimming
