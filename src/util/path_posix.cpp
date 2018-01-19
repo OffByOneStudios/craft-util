cd2d0add7f2a495b5379b664bc1ecec48b869c51
Clark Rinker <cgrinker@gmail.com>
2018-01-19 11:10:13 -0800

    Fixing Posix Path copypaste errors.

--- src/util/path_posix.cpp
+++ src/util/path_posix.cpp
@@ -456,7 +456,7 @@ std::string path::executable_path()
 
 void path::set_cwd(std::string const& path)
 {
-	chdir(path::dir(p).c_str());
+	chdir(path::dir(path).c_str());
 }
 
 std::string path::home_path()
