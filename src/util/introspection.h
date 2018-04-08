a616245e3f94830865ddcaf4ece573e5582ccf31
Mason Bially <mason.bially@gmail.com>
2018-04-08 14:53:34 -0700

    Adding new template template introspection.

--- src/util/introspection.h
+++ src/util/introspection.h
@@ -42,6 +42,16 @@ namespace stdext
 	};
 
 	//
+	// templates
+	//
+
+	template <class T, template <class...> class Template>
+	struct is_specialization : std::false_type {};
+
+	template <template <class...> class Template, class... Args>
+	struct is_specialization<Template<Args...>, Template> : std::true_type {};
+
+	//
 	// iterators
 	//
 
